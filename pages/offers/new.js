2ebc4e208a7b2e0e3fa4c64f1cfbd1ae9e82a5aa
Karol Nowakowski <karolnowakowski@MacBook-Pro-Karol.local>
2021-08-25 14:48:54 +0200

    support for the upload button

--- pages/offers/new.js
+++ pages/offers/new.js
@@ -127,8 +127,10 @@ export default function OfferNew() {
                 </div>
               </div>
               <div className="p-2 w-full">
-                <button disabled={formProcessing} className="disabled:opacity-50 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                {formProcessing ? 'Please wait...' : 'Submit offer'}
+                <button
+                  disabled={formProcessing}
+                  className="disabled:opacity-50 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  {formProcessing ? 'Please wait...' : 'Submit offer'}
                 </button>
               </div>
             </form>
