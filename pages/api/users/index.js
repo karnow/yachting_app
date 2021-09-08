0895f8938f9abd40691f50b181654712bf6f6e44
Karol Nowakowski <karolnowakowski@iMac-Karol.local>
2021-09-08 15:22:20 +0200

    preventing the creation of two identical users

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,20 +1,20 @@
 import createUser from 'services/users/create';
 // import authorizeUser from 'services/users/authorize';
 
-export default async (req,res)=>{
-    switch (req.method) {
-        case 'POST': {
-            try {
-                const payload =req.body;
-                const user= await createUser(payload);
+export default async (req, res) => {
+  switch (req.method) {
+    case 'POST': {
+      try {
+        const payload = req.body;
+        const user = await createUser(payload);
 
-                res.status(200).json({status: 'created', user});
-            } catch (error) {
-                res.status(422).json({status: 'not created', error});
-            }
-            break;
-        }
-        default:
-            res.status(400);
+        res.status(200).json({ status: 'created', user });
+      } catch (error) {
+        res.status(422).json({ status: 'not created', error: error.message });
+      }
+      break;
     }
-}
+    default:
+      res.status(400);
+  }
+};
--- services/users/create.js
+++ services/users/create.js
@@ -3,26 +3,38 @@ import Joi from 'joi';
 import crypto from 'crypto';
 
 const schema = Joi.object({
-    email: Joi.string().email().required(),
-    fullName: Joi.string().required(),
-    password: Joi.string().required()
+  email: Joi.string().email().required(),
+  fullName: Joi.string().required(),
+  password: Joi.string().required()
 });
+const checkEmail = async (email) => {
+  const existingUser = await airDB('users')
+    .select({ filterByFormula: `email="${email}"` })
+    .firstPage();
+
+  if (existingUser && existingUser[0]) {
+    throw new Error('email_taken');
+  }
+};
 
 const create = async (payload) => {
-    const {email, fullName, password} = await schema.validateAsync(payload);
-    const passwordSalt = crypto.randomBytes(16).toString('hex');
-    const passwordHash = crypto.pbkdf2Sync(password, passwordSalt, 1000, 64, `sha512`).toString(`hex`);
-    const user = await airDB('users').create([
-        {
-            fields:{
-                email,
-                fullName,
-                passwordSalt,
-                passwordHash,
-                role:'regular'
-            }
-        }
-    ])
-    return user;
-}
+  const { email, fullName, password } = await schema.validateAsync(payload);
+  await checkEmail(email);
+  const passwordSalt = crypto.randomBytes(16).toString('hex');
+  const passwordHash = crypto
+    .pbkdf2Sync(password, passwordSalt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  const user = await airDB('users').create([
+    {
+      fields: {
+        email,
+        fullName,
+        passwordSalt,
+        passwordHash,
+        role: 'regular'
+      }
+    }
+  ]);
+  return user;
+};
 export default create;
