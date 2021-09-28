057a8e00468b77d531aaafbe5d743325f28bf2fa
Karol Nowakowski <karolnowakowski@iMac-Karol.local>
2021-09-28 09:23:40 +0200

    Adding name <h1> for my offers site

--- pages/offers/my.js
+++ pages/offers/my.js
@@ -1,7 +1,7 @@
 import BaseLayout from 'components/BaseLayout';
 import OfferItem from 'components/OfferItem';
 import getForUser from 'services/offers/getForUser';
-import { getSession } from 'next-auth/client';
+import { getSession, session, useSession } from 'next-auth/client';
 
 export const getServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
@@ -17,16 +17,26 @@ export const getServerSideProps = async ({ req }) => {
 
   return {
     props: {
-      offers: offers
+      offers: offers,
+      session: session
     }
   };
 };
 
-export default function MyOffers({ offers }) {
+export default function MyOffers({ offers, session }) {
+  // console.log('sesja moja: ',session.user.name)
+  // const session = useSession(); 
+  // console.log('co z sesja:' ,session[0].user.name)
   return (
     <BaseLayout>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+              My offers - {session.user.name}
+            </h1>
+            <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+          </div>
           <div className="flex flex-wrap -m-4">
             {offers.length === 0 && (
               <div className="w-full text-center bg-yellow-100 py-4">
